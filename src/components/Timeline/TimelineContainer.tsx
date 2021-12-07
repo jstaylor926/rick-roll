f04359a662ff92df51c092066919500bfaaccf8a
Montjoy Application <54418577+jstaylor926@users.noreply.github.com>
2021-12-07 16:46:12 -0500

    hi

---
+++ src/components/Timeline/TimelineContainer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+const TimelineContainer: React.FC = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default TimelineContainer;
